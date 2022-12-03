9f924a078364ac0da8f5ff25d583dfdd5c5ac4a4
zhangmj <15669993907@163.com>
2022-12-03 15:35:42 +0800

    面板连续点击出现色彩，小问题解决。。。

--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -1,26 +1,11 @@
-import React, { memo, useEffect, useState } from 'react'
-import mjRequest from '@/services'
+import React, { memo } from 'react'
+import { HomeWrapper } from './style'
 
 const index = memo(() => {
-  const [highScore, setHighScore] = useState({})
-
-  useEffect(() => {
-    mjRequest.get({ url: '/home/highscore' }).then(res => {
-      console.log(res)
-      setHighScore(res)
-    })
-  }, [])
-
   return (
-    <div>
-      <h2>{highScore.title}</h2>
-      <h4>{highScore.subtitle}</h4>
-      <ul>
-        {highScore?.list?.map(item => {
-          return <li key={item.id}>{item.name}</li>
-        })}
-      </ul>
-    </div>
+    <HomeWrapper>
+      <h2>Home Page</h2>
+    </HomeWrapper>
   )
 })
 
