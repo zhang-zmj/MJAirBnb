15516037e8b3f340f380e85f490db5049153dd9c
zhangmj <15669993907@163.com>
2022-12-03 18:27:40 +0800

    首页底部Footer创建。。。

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { memo } from 'react'
 import { useRoutes } from 'react-router-dom'
+import AppFooter from './components/app-footer'
 import AppHeader from './components/app-header'
 import routes from './router'
 
@@ -8,7 +9,8 @@ const App = memo(() => {
     <div className="app">
       <AppHeader />
       <div className="page">{useRoutes(routes)}</div>
-      <div className="footer">footer</div>
+
+      <AppFooter />
     </div>
   )
 })
