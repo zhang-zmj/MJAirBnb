fe0cc55adc24fa0f4ccd2453ade82406358fb780
zhangmj <15669993907@163.com>
2022-12-03 14:42:48 +0800

    导航栏的配置，UI相关布局。。

--- src/components/app-header/c-cpns/header-right/style.js
+++ src/components/app-header/c-cpns/header-right/style.js
@@ -5,4 +5,46 @@ export const RightWrapper = styled.div`
   flex: 1;
   display: flex;
   justify-content: flex-end;
+
+  color: ${props => props.theme.text.primaryColor};
+  font-weight: 600;
+
+  .btns{
+    display: flex;
+    box-sizing: content-box;
+
+    .btn{
+      height: 18px;
+      line-height: 18px;
+      padding: 12px 15px;
+      border-radius: 22px;
+      cursor: pointer;
+      box-sizing: content-box;
+
+      &:hover{
+        background-color: #f5f5f5;
+      }
+    }
+  }
+
+  .profile{
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    width: 77px;
+    height: 42px;
+    margin-right: 24px;
+    box-sizing: border-box;
+    border: 1px solid #ccc;
+    border-radius: 25px;
+    background-color: #fff;
+    color: ${props => props.theme.text.primaryColor};
+    cursor: pointer;
+
+    ${props => props.theme.mixin.boxShadow}
+
+
+  }
+
+
 `
