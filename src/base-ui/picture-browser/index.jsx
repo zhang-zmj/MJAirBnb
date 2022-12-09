c7a242aae5df6af79eca975647768ccfa8c4e454
zhangmj <15669993907@163.com>
2022-12-09 22:39:19 +0800

    详情-图片浏览器的中间图片展示

--- src/base-ui/picture-browser/index.jsx
+++ src/base-ui/picture-browser/index.jsx
@@ -1,10 +1,15 @@
+import IconArrowLeft from '@/assets/svg/icon-arrow-left'
+import IconArrowRight from '@/assets/svg/icon-arrow-right'
 import IconClose from '@/assets/svg/icon_close'
 import PropTypes from 'prop-types'
-import React, { memo, useEffect } from 'react'
+import React, { memo, useEffect, useState } from 'react'
 import { BrowerWrapper } from './style'
 
 const PictureBrower = memo(props => {
   const { pictureUrls, closeClick } = props
+
+  const [currentIndex, setCurrentIndex] = useState(0)
+
   useEffect(() => {
     document.body.style.overflow = 'hidden'
     return () => {
@@ -17,15 +22,41 @@ const PictureBrower = memo(props => {
     if (closeClick) closeClick()
   }
 
+  function changeCurrentIndex(isNext = true) {
+    let newIndex = isNext ? currentIndex + 1 : currentIndex - 1
+    if (newIndex < 0) {
+      newIndex = pictureUrls.length - 1
+    }
+    if (newIndex > pictureUrls.length - 1) {
+      newIndex = 0
+    }
+    setCurrentIndex(newIndex)
+  }
+
   return (
     <BrowerWrapper>
       <div className="top">
-        <div className="close-btn" onClick={closeClick}>
+        <div className="close-btn" onClick={closeBtnClickHandle}>
           <IconClose />
         </div>
       </div>
-      <div className="list"></div>
-      <div className="indicator"></div>
+      
+      <div className="slider">
+        <div className="control">
+          <div className="btn left" onClick={e => changeCurrentIndex(false)}>
+            <IconArrowLeft width="77" height="77" />
+          </div>
+          <div className="btn right" onClick={e => changeCurrentIndex(true)}>
+            <IconArrowRight width="77" height="77" />
+          </div>
+        </div>
+
+        <div className="picture">
+          <img src={pictureUrls[currentIndex]} alt="" />
+        </div>
+      </div>
+
+      <div className="preview"></div>
     </BrowerWrapper>
   )
 })
--- src/base-ui/picture-browser/style.js
+++ src/base-ui/picture-browser/style.js
@@ -9,9 +9,10 @@ export const BrowerWrapper = styled.div`
   bottom: 0;
   background-color: #333;
 
-  /* opacity: 1;
+  //上下分成三部分
+  opacity: 1;
   display: flex;
-  flex-direction: column; */
+  flex-direction: column;
 
   .top{
     position: relative;
@@ -25,10 +26,62 @@ export const BrowerWrapper = styled.div`
     }
   }
 
+  .slider{
+    display: flex;
+    justify-content: center;
+    flex: 1;
+
+     //使用的是绝对定位，跟这个没关系
+    .control{
+      position: absolute;
+      z-index: 1;
+      left: 0;
+      right: 0;
+      top: 86px;
+      display: flex;
+      justify-content: space-between;
+      bottom: 0;
+      color: #fff;
+
+      .btn{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 83px;
+        height: 100%;
+        cursor: pointer;
+      }
+    }
+
+    .picture{
+      position: relative;
+      height: 100%;
+      overflow: hidden;
+      width: 100%;
+      max-width: 105vh;
+
+      img{
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        margin: 0 auto;
+        height: 100%;
+        user-select: none; //用于设置或检索是否允许用户选中文本
+
+      }
+
+    }
+
+  }
+
+  .preview{
+    height: 100px;
+    margin-top: 10px;
+    background-color: purple;
 
 
-
-
+  }
 
 
 `
